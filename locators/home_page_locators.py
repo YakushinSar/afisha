7eedb0bb2f71a2a3db5d1fb27404956e12060f34
YakushinSar <saratov482043162@gmail.com>
2023-12-02 18:59:01 +0400

    [+] header locators

--- locators/home_page_locators.py
+++ locators/home_page_locators.py
@@ -1,4 +1,10 @@
 from selenium.webdriver.common.by import By
 
 class HomePageLocators:
-    AFISHA_PEREDELANO = (By.XPATH,'//*[@class="header__logo"]')
+#header
+    AFISHA_PEREDELANO_HEDER= (By.XPATH,'//*[@class="header__logo"]')
+    ABOUT_US_TEXT = (By.XPATH,"//*[@class='header__nav-link'][contains(text(),'Об Афише')]")
+    SUPPORT_TEXT = (By.XPATH,"//*[@class='header__nav-link'][contains(text(),'Поддержка')]")
+    HELP_AFISHA_TEXT = (By.XPATH,"//*[@class='header__nav-link'][contains(text(),'Помочь Afisha')]")
+    LANGUAGE_SELECTOR = (By.XPATH,"//*[@aria-label='Выберите язык']")
+    CONTENT_BUTTON = (By.XPATH,"//*[@class ='button__content'][contains(text(),'Пользователь')]")
